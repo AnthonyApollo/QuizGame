5433789ed4ab6b70ec5444951735fcdd9d1fb67d
AnthonyApollo <anthonyapollo@outlook.com>
2023-10-22 17:54:55 -0300

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Anthony Apollo on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
