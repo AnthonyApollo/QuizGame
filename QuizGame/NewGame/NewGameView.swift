ddc5c0fad9bbfb6da5989e1e5b63201b2866f898
AnthonyApollo <anthonyapollo@outlook.com>
2023-10-22 21:29:18 -0300

    feat: Create `NewGameView`.

---
+++ QuizGame/NewGame/NewGameView.swift
@@ -0,0 +1,22 @@
+//
+//  NewGameView.swift
+//  QuizGame
+//
+//  Created by Anthony Apollo on 22/10/23.
+//
+
+import SwiftUI
+
+struct NewGameView: View {
+    var body: some View {
+        Text("New Game")
+            .font(.largeTitle)
+            .bold()
+    }
+}
+
+struct NewGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewGameView()
+    }
+}
